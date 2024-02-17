8a8d41c9dacabb625fa1801485fe30b00c43edde
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-02-17 18:48:59 -0300

    Added UpdateProductRequest

---
+++ src/main/java/com/tech/ada/java/lojadeva/dto/UpdateProductRequest.java
@@ -0,0 +1,20 @@
+package com.tech.ada.java.lojadeva.dto;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public record UpdateProductRequest(
+        String name,
+        String description,
+        BigDecimal price,
+        Integer inventoryQuantity,
+        String category) {
+
+    public UpdateProductRequest(String name, String description, BigDecimal price, Integer inventoryQuantity, String category){
+        this.name = Objects.requireNonNull(name, "O nome do produto é obrigatório.");
+        this.description = Objects.requireNonNull(description, "A descrição do produto é obrigatória.");
+        this.price = Objects.requireNonNull(price, "O preço do produto é obrigatório.");
+        this.inventoryQuantity = Objects.requireNonNull(inventoryQuantity, "A quantidade em estoque do produto é obrigatória.");
+        this.category = Objects.requireNonNull(category, "A categoria do produto é obrigatória.");
+    }
+}
