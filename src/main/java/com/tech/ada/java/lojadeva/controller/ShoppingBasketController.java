b392dee151dd45af0e9c09d5cac3ba7d531201e3
Ana Luiza Akiyama <akaori>
2024-02-17 16:21:22 -0300

    configure swagger

---
+++ src/main/java/com/tech/ada/java/lojadeva/controller/ShoppingBasketController.java
@@ -0,0 +1,23 @@
+package com.tech.ada.java.lojadeva.controller;
+
+import com.tech.ada.java.lojadeva.domain.ShoppingBasket;
+import com.tech.ada.java.lojadeva.repository.ShoppingBasketRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/shopping-basket")
+public class ShoppingBasketController {
+
+    private final ShoppingBasketRepository shoppingBasketRepository;
+
+    public ShoppingBasketController(ShoppingBasketRepository shoppingBasketRepository) {
+        this.shoppingBasketRepository = shoppingBasketRepository;
+    }
+
+    @GetMapping("/shopping-basket")
+    public List<ShoppingBasket> buscarTodos(){
+        return shoppingBasketRepository.findAll();
+    }
+}
