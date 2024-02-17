0ee33d3a569eab6e7f9a52bc31a3bcef8a295732
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-02-16 21:30:39 -0300

    Initial commit

---
+++ src/main/java/com/tech/ada/java/lojadeva/LojaDevaApplication.java
@@ -0,0 +1,13 @@
+package com.tech.ada.java.lojadeva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDevaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaDevaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tech/ada/java/lojadeva/LojaDevaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.ada.java.lojadeva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDevaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
