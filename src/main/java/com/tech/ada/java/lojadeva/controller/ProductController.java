b2a7dc4b5624358951e6c03127e222929db3a60d
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-02-17 18:12:28 -0300

    Added POST method for Product

--- src/main/java/com/tech/ada/java/lojadeva/controller/ProductController.java
+++ src/main/java/com/tech/ada/java/lojadeva/controller/ProductController.java
@@ -1,8 +1,15 @@
 package com.tech.ada.java.lojadeva.controller;
 
 import com.tech.ada.java.lojadeva.domain.Product;
+import com.tech.ada.java.lojadeva.dto.ProductRequest;
 import com.tech.ada.java.lojadeva.repository.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,14 +18,24 @@ import java.util.List;
 public class ProductController {
 
     private final ProductRepository productRepository;
+    private final ModelMapper modelMapper;
 
-    public ProductController(ProductRepository productRepository){
+    @Autowired
+    public ProductController(ProductRepository productRepository, ModelMapper modelMapper){
         this.productRepository = productRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<Product> registerProduct(@RequestBody ProductRequest productRequest) {
+        Product convertedProduct = modelMapper.map(productRequest, Product.class);
+        Product newProduct = productRepository.save(convertedProduct);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newProduct);
+
     }
 
     @GetMapping("/product")
     public List<Product> findAllProducts(){
-        List<Product> productsList = productRepository.findAll();
-        return productsList;
+        return productRepository.findAll();
     }
 }
