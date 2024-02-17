753b6318377a04377c03a040a7662ae0c5ae870e
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-02-17 20:27:16 -0300

    Added updateProduct method

--- src/main/java/com/tech/ada/java/lojadeva/dto/UpdateProductRequest.java
+++ src/main/java/com/tech/ada/java/lojadeva/dto/UpdateProductRequest.java
@@ -1,6 +1,7 @@
 package com.tech.ada.java.lojadeva.dto;
 
-import lombok.Getter;
+
+import com.tech.ada.java.lojadeva.domain.Product;
 
 import java.math.BigDecimal;
 import java.util.Objects;
@@ -18,5 +19,15 @@ public record UpdateProductRequest(
         this.price = Objects.requireNonNull(price, "O preço do produto é obrigatório.");
         this.inventoryQuantity = Objects.requireNonNull(inventoryQuantity, "A quantidade em estoque do produto é obrigatória.");
         this.category = Objects.requireNonNull(category, "A categoria do produto é obrigatória.");
+    }
+
+    public void updateProduct(Product product) {
+        if (product != null) {
+            product.setName(this.name());
+            product.setDescription(this.description());
+            product.setPrice(this.price());
+            product.setInventoryQuantity(this.inventoryQuantity());
+            product.setCategory(this.category());
+        }
     }
 }
