cd57b81e0763c105de782c33b5e452275637eb0c
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-02-17 18:23:50 -0300

    Added method find by id for Product

--- src/main/java/com/tech/ada/java/lojadeva/controller/ProductController.java
+++ src/main/java/com/tech/ada/java/lojadeva/controller/ProductController.java
@@ -7,12 +7,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController("/product")
 public class ProductController {
@@ -38,4 +36,11 @@ public class ProductController {
     public List<Product> findAllProducts(){
         return productRepository.findAll();
     }
+
+    @GetMapping("product/{id}")
+    public ResponseEntity<Optional<Product>> findProductById(@PathVariable Long id){
+        Optional<Product> product = productRepository.findById(id);
+        return ResponseEntity.ok().body(product);
+    }
 }
+
