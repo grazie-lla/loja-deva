03a9d7081fd0c2d38d71313dc1801e6d56de4769
Ana Luiza Akiyama <akaori>
2024-02-16 21:52:35 -0300

    adiciona fornecedor

--- src/main/java/com/tech/ada/java/lojadeva/domain/OrderManagement.java
+++ src/main/java/com/tech/ada/java/lojadeva/domain/OrderManagement.java
@@ -1,4 +1,13 @@
 package com.tech.ada.java.lojadeva.domain;
 
+import jakarta.persistence.Entity;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
 public class OrderManagement {
 }
