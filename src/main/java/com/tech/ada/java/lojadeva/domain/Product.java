be93de64e2611ebaf351a4378e1cf6093b8d3d11
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-02-16 22:25:53 -0300

    Refactored product entity

--- src/main/java/com/tech/ada/java/lojadeva/domain/Product.java
+++ src/main/java/com/tech/ada/java/lojadeva/domain/Product.java
@@ -18,9 +18,14 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String description;
+
     private BigDecimal price;
+
     private Integer inventoryQuantity;
+
     private String category;
 }
