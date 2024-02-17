a0b99464baf4f9a130d2b5cfd63f411c9c92fb1f
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-02-17 17:00:38 -0300

    added annotation repository

--- src/main/java/com/tech/ada/java/lojadeva/repository/ProductRepository.java
+++ src/main/java/com/tech/ada/java/lojadeva/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.tech.ada.java.lojadeva.repository;
 
 import com.tech.ada.java.lojadeva.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
