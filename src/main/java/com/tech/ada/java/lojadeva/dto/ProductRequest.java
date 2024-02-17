60b4fe3efe30d1bc3c651bd1433bbe9750c921ce
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-02-17 20:26:31 -0300

    Added constructor

--- src/main/java/com/tech/ada/java/lojadeva/dto/ProductRequest.java
+++ src/main/java/com/tech/ada/java/lojadeva/dto/ProductRequest.java
@@ -1,9 +1,9 @@
 package com.tech.ada.java.lojadeva.dto;
 
-import com.tech.ada.java.lojadeva.domain.Product;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Getter
 public class ProductRequest {
@@ -17,5 +17,12 @@ public class ProductRequest {
 
     private String category;
 
+    public ProductRequest(String name, String description, BigDecimal price, Integer inventoryQuantity, String category){
+        this.name = Objects.requireNonNull(name, "O nome do produto é obrigatório.");
+        this.description = Objects.requireNonNull(description, "A descrição do produto é obrigatória.");
+        this.price = Objects.requireNonNull(price, "O preço do produto é obrigatório.");
+        this.inventoryQuantity = Objects.requireNonNull(inventoryQuantity, "A quantidade em estoque do produto é obrigatória.");
+        this.category = Objects.requireNonNull(category, "A categoria do produto é obrigatória.");
+    }
 
 }
