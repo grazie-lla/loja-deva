8c9fa47cbdb548123878a99b9b5cc5d9baa08d4a
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-02-18 15:41:09 -0300

    Added price and inventoryQuantity validation

--- src/main/java/com/tech/ada/java/lojadeva/dto/UpdateProductDetailsRequest.java
+++ src/main/java/com/tech/ada/java/lojadeva/dto/UpdateProductDetailsRequest.java
@@ -1,9 +1,24 @@
 package com.tech.ada.java.lojadeva.dto;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public record UpdateProductDetailsRequest(
         String description,
         BigDecimal price,
         Integer inventoryQuantity) {
+
+    public UpdateProductDetailsRequest(String description, BigDecimal price, Integer inventoryQuantity){
+        this.description = description;
+
+        if (price != null && price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("O preço do produto não pode ser negativo.");
+        }
+        this.price = price;
+
+        if (inventoryQuantity != null && inventoryQuantity < 0) {
+            throw new IllegalArgumentException("A quantidade em estoque do produto não pode ser negativa.");
+        }
+        this.inventoryQuantity = inventoryQuantity;
+    }
 }
