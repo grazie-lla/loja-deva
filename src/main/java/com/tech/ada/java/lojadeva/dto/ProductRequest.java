2e2134ea5603121fb160952dd7f440627753750d
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-02-18 15:38:32 -0300

    Added price and inventoryQuantity validation

--- src/main/java/com/tech/ada/java/lojadeva/dto/ProductRequest.java
+++ src/main/java/com/tech/ada/java/lojadeva/dto/ProductRequest.java
@@ -17,11 +17,22 @@ public class ProductRequest {
 
     private String category;
 
-    public ProductRequest(String name, String description, BigDecimal price, Integer inventoryQuantity, String category){
+    public ProductRequest(String name, String description, BigDecimal price, Integer inventoryQuantity, String category) {
         this.name = Objects.requireNonNull(name, "O nome do produto é obrigatório.");
         this.description = Objects.requireNonNull(description, "A descrição do produto é obrigatória.");
-        this.price = Objects.requireNonNull(price, "O preço do produto é obrigatório.");
-        this.inventoryQuantity = Objects.requireNonNull(inventoryQuantity, "A quantidade em estoque do produto é obrigatória.");
+
+        Objects.requireNonNull(price, "O preço do produto é obrigatório.");
+        if (price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("O preço do produto não pode ser negativo.");
+        }
+        this.price = price;
+
+        Objects.requireNonNull(inventoryQuantity, "A quantidade em estoque do produto é obrigatória.");
+        if (inventoryQuantity < 0) {
+            throw new IllegalArgumentException("A quantidade em estoque do produto não pode ser negativa.");
+        }
+        this.inventoryQuantity = inventoryQuantity;
+
         this.category = Objects.requireNonNull(category, "A categoria do produto é obrigatória.");
     }
 
