c969399a1c5538338f454e665c8b6cfa392800a6
rewenila <32466162+rewenila@users.noreply.github.com>
2024-02-20 00:26:49 -0300

    added payment method validation

--- src/main/java/com/tech/ada/java/lojadeva/dto/OrderRequest.java
+++ src/main/java/com/tech/ada/java/lojadeva/dto/OrderRequest.java
@@ -2,21 +2,48 @@ package com.tech.ada.java.lojadeva.dto;
 
 import com.tech.ada.java.lojadeva.domain.PaymentMethod;
 import com.tech.ada.java.lojadeva.domain.Product;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class OrderRequest {
 
     private Long clientId;
     private List<Product> products;
     private BigDecimal total;
-    private PaymentMethod paymentMethod;
+    private String paymentMethod;
+
+    public OrderRequest(Long clientId,
+                        List<Product> products,
+                        BigDecimal total,
+                        String paymentMethod) {
+
+        this.clientId = Objects.requireNonNull(clientId, "Client id is mandatory.");
+
+        this.products = Objects.requireNonNull(products, "Products list is mandatory.");
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException("Products list must not be empty.");
+        }
+
+        this.total = Objects.requireNonNull(total, "Order total is mandatory.");
+        if (total.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Order total must not be negative.");
+        }
+
+        this.paymentMethod = Objects.requireNonNull(paymentMethod, "Payment method is mandatory.");
+        if (!isValidPaymentMethod(paymentMethod)) {
+            throw new IllegalArgumentException("Invalid payment method.");
+        }
+
+    }
+
+    private boolean isValidPaymentMethod(String paymentMethod) {
+        return Arrays.stream(PaymentMethod.values())
+                .anyMatch(enumValue -> enumValue.name().equalsIgnoreCase(paymentMethod));
+    }
 
 }
