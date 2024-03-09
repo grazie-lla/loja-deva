fb0dad005c0c84965e08bc031fc6b79607813235
Akaori <akiyamaanaluiza@gmail.com>
2024-03-08 22:01:42 -0300

    add verifications

--- src/test/java/com/tech/ada/java/lojadeva/controller/BasketItemControllerTest.java
+++ src/test/java/com/tech/ada/java/lojadeva/controller/BasketItemControllerTest.java
@@ -14,9 +14,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.tech.ada.java.lojadeva.domain.BasketItem;
 import org.springframework.http.HttpStatus;
@@ -86,11 +87,13 @@ class BasketItemControllerTest {
         List<BasketItem> expectedItems = Arrays.asList(basketItem1, basketItem2);
         when(basketItemService.findAllItems()).thenReturn(expectedItems);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/basket-item/items").
+        var response = mockMvc.perform(MockMvcRequestBuilders.get("/basket-item/items").
                 contentType(MediaType.APPLICATION_JSON).
                 content(asJsonString(expectedItems))).
                 andDo(MockMvcResultHandlers.print());
 
+        response.andExpect(status().isOk());
+        response.andExpect(jsonPath("$.[0].id", equalTo(1)));
         verify(basketItemService, times(1)).findAllItems();
     }
 
@@ -100,11 +103,13 @@ class BasketItemControllerTest {
         Optional<BasketItem> expectedItem = Optional.of(basketItem1);
         when(basketItemService.findItemById(id)).thenReturn(expectedItem);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/basket-item/1").
+        var response = mockMvc.perform(MockMvcRequestBuilders.get("/basket-item/1").
                         contentType(MediaType.APPLICATION_JSON).
                         content(asJsonString(expectedItem)))
                 .andDo(MockMvcResultHandlers.print());
 
+        response.andExpect(status().isOk());
+        response.andExpect(jsonPath("$.id", equalTo(1)));
         verify(basketItemService, times(1)).findItemById(id);
     }
 
@@ -114,12 +119,12 @@ class BasketItemControllerTest {
         Optional<BasketItem> expectedItem = Optional.empty();
         when(basketItemService.findItemById(id)).thenReturn(expectedItem);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/basket-item/3").
+        var response = mockMvc.perform(MockMvcRequestBuilders.get("/basket-item/3").
                         contentType(MediaType.APPLICATION_JSON).
                         content(asJsonString(expectedItem))).
-                        andDo(MockMvcResultHandlers.print()).
-                        andExpect(status().isNotFound());
+                        andDo(MockMvcResultHandlers.print());
 
+        response.andExpect(status().isNotFound());
         verify(basketItemService, times(1)).findItemById(id);
     }
 
@@ -129,12 +134,14 @@ class BasketItemControllerTest {
         List<BasketItem> expectedItems = Arrays.asList(basketItem1, basketItem2);
         when(basketItemService.findItemsByShoppingBasketId(shoppingBasketId)).thenReturn(expectedItems);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/basket-item/items/by-basket").
+        var response = mockMvc.perform(MockMvcRequestBuilders.get("/basket-item/items/by-basket").
                         param("shoppingBasketId", "1").
                         contentType(MediaType.APPLICATION_JSON).
                         content(asJsonString(expectedItems))).
                         andDo(MockMvcResultHandlers.print());
 
+        response.andExpect(status().isOk());
+        response.andExpect(jsonPath("$.[0].id", equalTo(1)));
         verify(basketItemService, times(1)).findItemsByShoppingBasketId(shoppingBasketId);
     }
 
@@ -142,10 +149,12 @@ class BasketItemControllerTest {
     void createItemHttpRequest() throws Exception {
         when(basketItemService.createItem(Mockito.any())).thenReturn(basketItem1);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/basket-item").
+        var response = mockMvc.perform(MockMvcRequestBuilders.post("/basket-item").
                 contentType(MediaType.APPLICATION_JSON).
                 content(asJsonString(basketItemRequest))).andExpect(status().isCreated());
 
+        response.andExpect(status().isCreated());
+        response.andExpect(jsonPath("$.id", equalTo(1)));
         verify(basketItemService, times(1)).createItem(Mockito.any());
     }
 
@@ -154,11 +163,13 @@ class BasketItemControllerTest {
         Long id = 1L;
         when(basketItemService.deleteItem(id)).thenReturn(true);
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/basket-item/1").
+        var response = mockMvc.perform(MockMvcRequestBuilders.delete("/basket-item/1").
                         contentType(MediaType.APPLICATION_JSON).
                         content(asJsonString(basketItem1)))
                 .andDo(MockMvcResultHandlers.print());
 
+        response.andExpect(status().isOk());
+        response.andExpect(content().string("Item deletado com sucesso!"));
         verify(basketItemService, times(1)).deleteItem(id);
     }
 
@@ -167,12 +178,12 @@ class BasketItemControllerTest {
         Long id = 3L;
         when(basketItemService.deleteItem(id)).thenReturn(false);
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/basket-item/3").
+        var response = mockMvc.perform(MockMvcRequestBuilders.delete("/basket-item/3").
                         contentType(MediaType.APPLICATION_JSON).
                         content(asJsonString(basketItem1)))
-                        .andDo(MockMvcResultHandlers.print()).
-                        andExpect(status().isNotFound());
+                        .andDo(MockMvcResultHandlers.print());
 
+        response.andExpect(status().isNotFound());
         verify(basketItemService, times(1)).deleteItem(id);
     }
 
@@ -186,11 +197,13 @@ class BasketItemControllerTest {
         expectedItem.setProduct(new Product());
         when(basketItemService.updateItemQuantity(id, 5)).thenReturn(expectedItem);
 
-        mockMvc.perform(MockMvcRequestBuilders.patch("/basket-item/2").
+        var response = mockMvc.perform(MockMvcRequestBuilders.patch("/basket-item/2").
                         contentType(MediaType.APPLICATION_JSON).
                         content(asJsonString(updateItemQuantityRequest)))
                 .andDo(MockMvcResultHandlers.print());
 
+        response.andExpect(status().isOk());
+        response.andExpect(jsonPath("$.id", equalTo(2)));
         verify(basketItemService, times(1)).updateItemQuantity(id, 5);
     }
 
@@ -199,12 +212,13 @@ class BasketItemControllerTest {
         Long id = 3L;
         when(basketItemService.updateItemQuantity(id, 5)).thenReturn(null);
 
-        mockMvc.perform(MockMvcRequestBuilders.patch("/basket-item/3").
+        var response = mockMvc.perform(MockMvcRequestBuilders.patch("/basket-item/3").
                         contentType(MediaType.APPLICATION_JSON).
                         content(asJsonString(updateItemQuantityRequest)))
                         .andDo(MockMvcResultHandlers.print()).
                         andExpect(status().isNotFound());
 
+        response.andExpect(status().isNotFound());
         verify(basketItemService, times(1)).updateItemQuantity(id, 5);
     }
 }
